8afdc0350a1843f4dec5dea71b976389fecb2b02
Surin Lee <leesurin99@gmail.com>
2022-05-16 19:12:06 +0900

    [update]프로젝트 생성

---
+++ config/settings/deploy.py
@@ -0,0 +1,12 @@
+from config.settings.base import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3.deploy'),
+    }
+}
