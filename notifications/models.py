51e88c4590173f26b9dd362cfd4e1f4b27bfa57a
leeceo97 <leeceo97@naver.com>
2022-06-14 16:08:07 +0900

    (feat)initial notification

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from accounts.models import User
 
 
+class Notification(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    message = models.TextField()
+
+@receiver(post_save, sender=User)
+def create_user_notification(sender, instance, created, **kwargs):
+    if created:
+        Notification.objects.create(user=instance, message=f"{instance.nickname}님 가입을 축하합니다.")
