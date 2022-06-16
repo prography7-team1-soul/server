a66c6f4fda3c2e13687d155414707d97ca3421fc
Surin Lee <leesurin99@gmail.com>
2022-06-16 20:20:06 +0900

    (fix)refactor code & (add)swagger description

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -8,10 +8,32 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             'uuid',
             'nickname',
+        ]
+
+        read_only_fields = ['uuid', 'nickname']
+
+    def create(self, validated_data):
+        request_data = self.context.get('request')
+        # uuid가 없으면 None으로 반환
+        uuid = request_data.headers.get('uuid', None)
+        # get으로 가져오면 오류로 다운될 수 있음
+        user = User.objects.filter(uuid=uuid).first()
+        if user:
+            raise serializers.ValidationError({'error_message': '이미 가입된 계정이 존재합니다.'})
+        validated_data['uuid'] = uuid
+        return super().create(validated_data)
+
+
+class UserBookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
             'club_bookmarks',
             'chatroom_bookmarks',
             'article_bookmarks',
         ]
+
+        read_only_fields = ['club_bookmarks', 'chatroom_bookmarks', 'article_bookmarks']
 
 
 class UserClubBookmarkSerializer(serializers.ModelSerializer):
@@ -36,28 +58,3 @@ class UserArticleBookmarkSerializer(serializers.ModelSerializer):
         fields = [
             'article_bookmarks',
         ]
-
-
-class UserSignUpSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            'uuid',
-            'nickname',
-            'club_bookmarks',
-            'chatroom_bookmarks',
-            'article_bookmarks',
-        ]
-
-        read_only_fields = ['uuid', 'nickname', 'club_bookmarks', 'chatroom_bookmarks', 'article_bookmarks']
-
-    def create(self, validated_data):
-        request_data = self.context.get('request')
-        # uuid가 없으면 None으로 반환
-        uuid = request_data.headers.get('uuid', None)
-        # get으로 가져오면 오류로 다운될 수 있음
-        user = User.objects.filter(uuid=uuid).first()
-        if user:
-            raise serializers.ValidationError({'error_message': '이미 가입된 계정이 존재합니다.'})
-        validated_data['uuid'] = uuid
-        return super().create(validated_data)
--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,66 @@
 from rest_framework import mixins
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from accounts.models import User
 from accounts.permissions import IsAuthenticated
-from accounts.serializer import UserSerializer, UserSignUpSerializer, UserClubBookmarkSerializer, \
-    UserChatroomBookmarkSerializer, UserArticleBookmarkSerializer
+from accounts.serializer import UserSerializer, UserClubBookmarkSerializer, \
+    UserChatroomBookmarkSerializer, UserArticleBookmarkSerializer, UserBookmarkSerializer
 from accounts.filters import BookmarkFilter
 
 
-class UserDetailViewSet(ModelViewSet):
+class UserDetailViewSet(mixins.RetrieveModelMixin, GenericViewSet):
+    """
+    유저 개인 page를 조회하는 API
+
+    ---
+    ## `/api/users/<pk>`
+    ## 요청 메소드
+        - GET 메소드만 가능합니다.
+    ## 에러 메시지
+        - 인증된 유저가 아닐 경우 401 Unauthorized 에러가 발생합니다.
+    ## 내용
+        - uuid : 유저 개인 uuid 값
+        - nickname : 유저 개인 nickname
+    """
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
 
 
 class SignUpViewSet(mixins.CreateModelMixin, GenericViewSet):
-    serializer_class = UserSignUpSerializer
+    """
+    유저 회원 가입 page 생성 API
+
+    ---
+    ## `/api/signup`
+    ## 요청 메소드
+        - POST 메소드만 가능합니다.
+    ## 에러 메시지
+        - 이미 가입된 값일 경우 '이미 가입된 계정입니다' 라는 에러메시지 발생
+    ## 내용
+        - uuid : 유저 개인 uuid 값
+    """
+    serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
-class UserBookmarkViewSet(ReadOnlyModelViewSet):
+class UserBookmarkViewSet(mixins.ListModelMixin, GenericViewSet):
+    """
+    유저 북마크 조회 page 생성 API
+
+    ---
+    ## `api/users/<user_pk>/bookmarks?app=<app_name>`
+        - parameter로 article, chatroom, club 전달 시 각각의 북마크 리스트가 전달됩니다.
+        - parameter가 전달되지 않으면 모든 앱의 북마크 리스트가 전달됩니다.
+    ## 요청 메소드
+        - GET 메소드만 가능합니다.
+    ## 에러 메시지
+        - 비인증 유저에 대해서 401 Unauthorized 에러가 발생합니다.
+    ## 내용
+        - article_bookmarks: 소울 후기 북마크 리스트
+        - chatroom_bookmarks: 오픈톡방 북마크 리스트
+        - club_bookmarks: 동아리 북마크 리스트
+    """
     permission_classes = [IsAuthenticated]
     filter_backends = [BookmarkFilter]
 
@@ -30,6 +71,8 @@ class UserBookmarkViewSet(ReadOnlyModelViewSet):
             return UserChatroomBookmarkSerializer
         elif self.request.query_params.get('app') == 'article':
             return UserArticleBookmarkSerializer
+        else:
+            return UserBookmarkSerializer
 
     def get_queryset(self):
         return User.objects.filter(pk=self.kwargs['user_pk'])
@@ -37,9 +80,14 @@ class UserBookmarkViewSet(ReadOnlyModelViewSet):
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         key = request.query_params['app']
-        response = {
-            key+'_bookmark_list': response.data,
-        }
+        if key is None:
+            response = {
+                'all_bookmark_list': response.data,
+            }
+        else:
+            response = {
+                key+'_bookmark_list': response.data,
+            }
         return Response(response)
 
 
