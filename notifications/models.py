df5c24a3630e98462d20eb6ee79fe76b65c4826f
leeceo97 <leeceo97@naver.com>
2022-06-18 21:40:05 +0900

    (fix)signals

--- notifications/models.py
+++ notifications/models.py
@@ -15,7 +15,7 @@ class Notification(models.Model):
 def create_user_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance, message=f"{instance.nickname}님 가입을 축하합니다.")
-        registration_token = ''
+        registration_token = 'cM9Uwmb3JU_DkeIewiua0C:APA91bHx7_JoHBKp4oY3I2CcSVgc9wyhfkx9w22c6ZaA17wWg0514MCnyDvq0pqL3VoPriD9U6XFYRKA-31Cn_BdwsyuE4pqyvbxSykSJFVmU9OWMluluMwIvVjb-G81UJTPCn5yVpPM'
         message = messaging.Message(
             notification=messaging.Notification(
                 title='안녕하세요 테스트 메시지 입니다.',
