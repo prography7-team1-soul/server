fe05dc44a0dced89b6e69e9b6e4df7870b83670c
leeceo97 <leeceo97@naver.com>
2022-06-21 02:48:00 +0900

    (feat)notification api

--- notifications/models.py
+++ notifications/models.py
@@ -12,16 +12,32 @@ class Notification(models.Model):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     message = models.TextField()
 
+    class PageType(models.TextChoices):
+        club = 'club'
+        chat_room = 'chat_room'
+        article = 'article'
+        profile = 'profile'
+
+    page_type = models.CharField(
+        max_length=15,
+        choices=PageType.choices,
+        blank=True,
+        default=''
+    )
+    page_id = models.PositiveIntegerField(blank=True, default=True)
+    is_read = models.BooleanField(default=False)
+
 
 @receiver(post_save, sender=User)
 def create_user_notification(sender, instance, created, **kwargs):
     if created:
-        Notification.objects.create(user=instance, message=f"{instance.nickname}님 가입을 축하합니다.")
+        notification = Notification.objects.create(user=instance, message=f"{instance.nickname}님 가입을 축하합니다.",
+                                                   page_type='profile', page_id=instance.id)
         registration_token = instance.fcm_token
         message = messaging.Message(
             notification=messaging.Notification(
-                title='안녕하세요 테스트 메시지 입니다.',
-                body='테스트 메시지!',
+                title='가입을 축하합니다.',
+                body=f'{instance.nickname}',
             ),
             token=registration_token,
         )
@@ -34,15 +50,16 @@ def create_user_notification(sender, instance, created, **kwargs):
 def save_club_notification(sender, instance, created, **kwargs):
     users = User.objects.all()
     for user in users:
-        if instance in user.club_bookmarks:
-            Notification.objects.create(user=user, message='club')
+        if instance in user.club_bookmarks.all():
+            Notification.objects.create(user=user, message='club',
+                                        page_type='club', page_id=instance.id)
             registration_token = user.fcm_token
             message = messaging.Message(
                 notification=messaging.Notification(
-                    title='클럽 테스트 메시지입니다.',
+                    title=f'{instance.name}동아리의 정보가 변경됐어요!',
                     body='테스트 메시지!',
                 ),
                 token=registration_token,
             )
-        response = messaging.send(message)
-        print('Successfully sent message:', response)
+            response = messaging.send(message)
+            print('Successfully sent message:', response)
