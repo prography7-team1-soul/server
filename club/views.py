d06cf433c6df228dc5d3f51c27bba4f72393caa9
leeceo97 <leeceo97@naver.com>
2022-06-20 20:10:17 +0900

    (docs)api documentations

--- club/views.py
+++ club/views.py
@@ -1,4 +1,5 @@
-from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema, no_body
 from rest_framework import viewsets, status
 from club.models import Club
 from club.serializers import ClubSummarizeSerializer
@@ -19,7 +20,8 @@ class ClubViewSet(viewsets.ReadOnlyModelViewSet):
         elif self.action == 'bookmark':
             return None
 
-    @swagger_auto_schema(operation_summary="it 동아리 리스트 API")
+    @swagger_auto_schema(operation_summary="it 동아리 리스트 API", request_body=no_body,
+                         operation_description="- 헤더는 필요없어요!",)
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         response = {
@@ -27,7 +29,11 @@ class ClubViewSet(viewsets.ReadOnlyModelViewSet):
         }
         return Response(response, status=status.HTTP_200_OK)
 
-    @swagger_auto_schema(operation_summary="it 동아리 상세보기 API")
+    @swagger_auto_schema(operation_summary="it 동아리 상세보기 API", request_body=no_body,
+                         manual_parameters=[
+                             openapi.Parameter('id', openapi.IN_PATH, description="반드시 url에 식별자 id값이 필요합니다.",
+                                               type=openapi.TYPE_STRING)
+                         ])
     def retrieve(self, request, *args, **kwargs):
         response = super().retrieve(request, *args, **kwargs)
         response = {
@@ -35,7 +41,12 @@ class ClubViewSet(viewsets.ReadOnlyModelViewSet):
         }
         return Response(response, status=status.HTTP_200_OK)
 
-    @swagger_auto_schema(operation_summary="it 동아리 북마크 on/off API", operation_description="request header에 uuid 필수!")
+    @swagger_auto_schema(operation_summary="it 동아리 북마크 on/off API", operation_description="request header에 uuid 필수!",
+                         request_body=no_body,
+                         manual_parameters=[
+                             openapi.Parameter('uuid', openapi.IN_HEADER, description="인증을 위해 반드시 헤더에 필요합니다.",
+                                               type=openapi.TYPE_STRING),
+                         ])
     @action(methods=['post'], detail=True)
     def bookmark(self, request, pk):
         club = self.get_object()
