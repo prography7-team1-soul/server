d44c35e84ac45b1e81500956fba187893bb846f6
leeceo97 <leeceo97@naver.com>
2022-07-06 22:37:30 +0900

    (add)educations

--- educations/serializers.py
+++ educations/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from accounts.models import User
 from educations.models import Education, RecruitmentField
 
 
@@ -13,7 +14,7 @@ class RecruitmentSerializer(serializers.ModelSerializer):
 
 class EducationSummarizeSerializer(serializers.ModelSerializer):
     recruitment_fields = RecruitmentSerializer(many=True, read_only=True)
-
+    is_bookmark = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Education
         fields = (
@@ -22,12 +23,25 @@ class EducationSummarizeSerializer(serializers.ModelSerializer):
             'education_description',
             'recruitment_fields',
             'image',
+            'is_bookmark',
         )
+
+    def get_is_bookmark(self, obj):
+        user = self.context.get("request").user
+        print(user)
+        if user != None:
+            is_bookmark = User.objects.filter(id=user.id, education_bookmarks__in=[obj]).first()
+            if is_bookmark is None:
+                return False
+            else:
+                return True
+        else:
+            return False
 
 
 class EducationDetailSerializer(serializers.ModelSerializer):
     recruitment_fields = RecruitmentSerializer(many=True, read_only=True)
-
+    is_bookmark = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Education
         fields = (
@@ -44,4 +58,17 @@ class EducationDetailSerializer(serializers.ModelSerializer):
             'education_period',
             'home_url',
             'sns',
+            'is_bookmark',
         )
+
+    def get_is_bookmark(self, obj):
+        user = self.context.get("request").user
+        print(user)
+        if user != None:
+            is_bookmark = User.objects.filter(id=user.id, education_bookmarks__in=[obj]).first()
+            if is_bookmark is None:
+                return False
+            else:
+                return True
+        else:
+            return False
