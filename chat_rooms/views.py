b077492e4691044c3c8a75bbafaad832844005a8
leeceo97 <leeceo97@naver.com>
2022-07-15 02:17:21 +0900

    (add)api documentation

--- chat_rooms/views.py
+++ chat_rooms/views.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema, no_body
 from rest_framework import status, mixins
 from rest_framework.decorators import api_view
 from rest_framework.viewsets import GenericViewSet
@@ -32,11 +33,13 @@ class ChatRoomListView(mixins.ListModelMixin, GenericViewSet):
         else:
             return Category.objects.all()
 
+    @swagger_auto_schema(operation_summary="오픈채팅방 리스트",
+                         operation_description="헤더있어도 되고 없어도 됩니당. 요청 보내면 전체리스트 반환되고 뒤에 파라미터는 ?category=[개발자, 디자이너, "
+                                               "기획자]로 보내면됨! 셋중 하나로!!  나중에 중분류 확정되면 그때 ㄱㄱ",
+                         request_body=no_body)
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         response = {
             'chat_rooms_list': response.data
         }
         return Response(response, status=status.HTTP_200_OK)
-
-
